c58dab6b6d1dd716b95f4b03f23d2a16c4f7ac31
charlie <cmordant1@gmail.com>
2017-12-02 15:20:54 +0100

    added site generation

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   git checkout -B "$TRAVIS_BRANCH"
-  mvn -f .mvn/parent/pom.xml --batch-mode release:prepare -P release,maven-central --settings travis/settings.xml
-  mvn -f .mvn/parent/pom.xml --batch-mode release:perform -P release,maven-central --settings travis/settings.xml
+  mvn -f .mvn/parent/pom.xml --batch-mode release:prepare -P release --settings travis/settings.xml
+  mvn -f .mvn/parent/pom.xml --batch-mode release:perform -P release,nexus-pro --settings travis/settings.xml
 fi
